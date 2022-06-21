ead3b162e18e4930e262a4d3173dde04dff21996
vicky1312 <victoryabayomi@gmail.com>
2022-06-21 16:15:16 +0100

    crud project

--- I4G000838FN4/I4G000838FN4/urls.py
+++ I4G000838FN4/I4G000838FN4/urls.py
@@ -20,5 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("blog/", include("blog.urls", namespace="blog")),
+    path("blog/", include("blog.urls", Namespace="blog")),
 ]
